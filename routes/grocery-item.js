037a6e08970e5d4002fc1a1d1b90786dfd607ef6
Zarsalan <118849721+zarsalan@users.noreply.github.com>
2024-11-28 10:50:24 -0500

    Ajout des routes get pour items et recipes

--- routes/grocery-item.js
+++ routes/grocery-item.js
@@ -32,9 +32,15 @@ router.get("/get", async (req, res, next) => {
 		if (category) {
 			groceryItems = await GroceryItem.find({
 				_id: { $in: category.items },
+			}).populate({
+				path: "category",
+				select: "name description",
 			});
 		} else {
-			groceryItems = await GroceryItem.find();
+			groceryItems = await GroceryItem.find().populate({
+				path: "category",
+				select: "name description",
+			});
 		}
 
 		if (!groceryItems || groceryItems.length === 0) {
@@ -59,6 +65,62 @@ router.get("/get", async (req, res, next) => {
 		}
 
 		res.json(groceryItems);
+	} catch (err) {
+		next(err);
+	}
+});
+
+router.get("/get/:id", async (req, res, next) => {
+	try {
+		const groceryItem = await GroceryItem.findById(req.params.id);
+		if (!groceryItem) {
+			return res.status(404).json({
+				message: "Item non trouvé",
+			});
+		}
+
+		await groceryItem.populate({
+			path: "category",
+			select: "name description",
+		});
+
+		res.json(groceryItem);
+	} catch (err) {
+		next(err);
+	}
+});
+
+router.get("/get-items", async (req, res, next) => {
+	try {
+		const ids = req.query.ids;
+
+		let items;
+		if (ids) {
+			items = await GroceryItem.find({
+				_id: { $in: ids },
+			}).populate({
+				path: "category",
+				select: "name description",
+			});
+		} else {
+			items = await GroceryItem.find().populate({
+				path: "category",
+				select: "name description",
+			});
+		}
+
+		res.json(items);
+	} catch (err) {
+		next(err);
+	}
+});
+
+router.get("/get-categories", async (req, res, next) => {
+	try {
+		const categories = await Category.find();
+		categories.sort((a, b) => a.name.localeCompare(b.name));
+
+		res.json(categories);
 	} catch (err) {
 		next(err);
 	}
--- routes/recipe.js
+++ routes/recipe.js
@@ -66,4 +66,83 @@ router.get("/get", async (req, res, next) => {
 	}
 });
 
+router.get("/get/:id", async (req, res, next) => {
+	try {
+		const recipe = await Recipe.findById(req.params.id);
+		if (!recipe) {
+			console.log("Recette non trouvée: ", req.params.id);
+			return res.status(404).json({
+				message: "Recette non trouvée",
+			});
+		}
+
+		await recipe.populate([
+			{
+				path: "category",
+				select: "name description",
+			},
+			{
+				path: "ingredients.item",
+				select: "name description",
+			},
+		]);
+		res.json(recipe);
+	} catch (err) {
+		next(err);
+	}
+});
+
+router.get("/get-recipes", async (req, res, next) => {
+	try {
+		const ids = req.query.ids;
+
+		let recipes;
+		if (ids) {
+			recipes = await Recipe.find({
+				_id: { $in: ids },
+			}).populate([
+				{
+					path: "category",
+					select: "name description",
+				},
+				{
+					path: "ingredients.item",
+					select: "name description",
+				},
+			]);
+		} else {
+			recipes = await Recipe.find().populate([
+				{
+					path: "category",
+					select: "name description",
+				},
+				{
+					path: "ingredients.item",
+					select: "name description",
+				},
+			]);
+		}
+
+		if (!recipes || recipes.length === 0) {
+			return res.status(404).json({
+				message: "Aucune recette trouvée",
+			});
+		}
+
+		res.json(recipes);
+	} catch (err) {
+		next(err);
+	}
+});
+
+router.get("/get-categories", async (req, res, next) => {
+	try {
+		const categories = await Category.find();
+		categories.sort((a, b) => a.name.localeCompare(b.name));
+		res.json(categories);
+	} catch (err) {
+		next(err);
+	}
+});
+
 export default router;
